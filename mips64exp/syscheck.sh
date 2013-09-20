416d2fe77668947da69c424e8f1c2be32f19972e
Kyle Isom <kyle@tyrfingr.is>
2013-09-20 00:34:15 -0600

    Add started syscall test code.

---
+++ mips64exp/syscheck.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "$(uname -s)" != "OpenBSD" ]; then
+	echo "[!] This code is targeted to OpenBSD, and you are not on"
+	echo "    an OpenBSD system. *This code will probably not work!*"
+	exit 1
+elif [ "$(arch -s)" != "mips64" ]; then
+	echo "[!] This code is written in MIPS64 assembly, and you are"
+	echo "    not on a MIPS64 machine. This code will not work."
+	exit 1
+fi
